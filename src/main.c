260bf7d931c05671be05e70e62f2fc61bda6540d
Laszlo Graf <graf.laszlo@gmail.com>
2025-02-24 11:32:06 +0200

    Az alap APR dolgok, kicsit bővebben.

--- src/main.c
+++ src/main.c
@@ -1,9 +1,44 @@
-#include <apr_general.h>
+/**
+ * apr tutorial sample code
+ * http://dev.ariel-networks.com/apr/
+ */
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
 
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <apr_general.h>
+#include <apr_pools.h>
+
+#define MEM_ALLOC_SIZE		1024
+
+/**
+ * memory pool sample code
+ * @remark Error checks omitted
+ */
 int main(int argc, const char *argv[]) {
+    apr_status_t rv;
     apr_pool_t *mp;
-    apr_initialize();
+    char *buf1;
+    char *buf2;
+    /* per-process initialization */
+    rv = apr_initialize();
+    if (rv != APR_SUCCESS) {
+        assert(0);
+        printf("Error initializing the APR.\n");
+        return -1;
+    }
+    /* create a memory pool. */
     apr_pool_create(&mp, NULL);
+    printf("Main memory pool created.\n");
+    /* allocate memory chunks from the memory pool */
+    buf1 = apr_palloc(mp, MEM_ALLOC_SIZE);
+    buf2 = apr_palloc(mp, MEM_ALLOC_SIZE);
+    /* destroy the memory pool. These chunks above are freed by this */
+    apr_pool_destroy(mp);
     apr_terminate();
     return 0;
 }
