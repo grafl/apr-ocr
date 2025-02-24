791317e04cd1a6b16a83e2c22c38245616d5f94b
Laszlo Graf <graf.laszlo@gmail.com>
2025-02-24 15:09:09 +0200

    Az első library.

---
+++ include/teszt.h
@@ -0,0 +1,8 @@
+#ifndef APR_OCR_TESZT_H
+#define APR_OCR_TESZT_H
+
+#include <apr_general.h>
+
+APR_DECLARE(void) teszt();
+
+#endif //APR_OCR_TESZT_H
---
+++ src/teszt.c
@@ -0,0 +1,5 @@
+#include "teszt.h"
+
+APR_DECLARE(void) teszt() {
+
+}
