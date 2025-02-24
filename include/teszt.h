bf49a3099040757259278ed84c26529f88a1f041
Laszlo Graf <graf.laszlo@gmail.com>
2025-02-24 15:51:39 +0200

    A teszt eljárás.

--- include/teszt.h
+++ include/teszt.h
@@ -1,8 +1,10 @@
 #ifndef APR_OCR_TESZT_H
 #define APR_OCR_TESZT_H
 
+#include <stdio.h>
+
 #include <apr_general.h>
 
-APR_DECLARE(void) teszt();
+APR_DECLARE(void) teszt(apr_pool_t *p_mp);
 
 #endif //APR_OCR_TESZT_H
--- src/teszt.c
+++ src/teszt.c
@@ -1,5 +1,6 @@
 #include "teszt.h"
 
-APR_DECLARE(void) teszt() {
-
+APR_DECLARE(void) teszt(apr_pool_t *p_mp) {
+    char *buf1 = apr_pstrdup(p_mp, "Laci");
+    printf("%s\n", buf1);
 }
